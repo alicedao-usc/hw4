e97f8f1b7953443dd646d32b244134780d704c12
Alice Dao <alicedao@usc.edu>
2022-11-18 06:24:07 +0000

    init commit

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -5,9 +5,68 @@ using namespace std;
 // You may add any prototypes of helper functions here
 
 
+int leaf(Node * root)
+{
+	// if (root == NULL)
+	// {
+	// 	return 0;
+	// }
+    if (root -> left == nullptr && root -> right == nullptr)
+    {
+        return 0;
+    }
+    else if (root -> left == nullptr)
+    {
+				return 1 + leaf(root -> right);
+
+    }
+		else
+		{
+				return 1 + leaf(root->left);
+
+		}
+  return 0;
+}
+
+bool isSameHeight(int maxheight, int currHeight, Node* n)
+{
+
+    if (n -> left == nullptr && n -> right == nullptr)
+    {
+        if (currHeight != maxheight)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    if (n -> left == nullptr)
+    {
+        return isSameHeight(maxheight, currHeight + 1, n -> right);
+    }
+    else if (n -> right == nullptr)
+    {
+        return isSameHeight(maxheight,currHeight + 1, n -> left);
+    }
+    else
+    {
+        return isSameHeight(maxheight, currHeight + 1, n-> left) && isSameHeight(maxheight,currHeight + 1, n -> right);
+
+    }
+	
+}   
+
 bool equalPaths(Node * root)
 {
+		if (root == NULL)
+		{
+			return true;
+		}
     // Add your code below
+    int height =  leaf(root);
+    return isSameHeight(height, 0, root);
 
 }
 
